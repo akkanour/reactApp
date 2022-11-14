9fed4c3564bbac72bc253581b6e74743c3a1f7d5
akkanour <akkaa01001@gmail.com>
2022-11-14 18:41:05 +0100

    cccc

--- src/App.js
+++ src/App.js
@@ -2,18 +2,16 @@
 import React from "react";
 import "swiper/css/bundle";
 import "./style.css";
-
 import Slider  from './Components/Content/Slides/slider';
 import Navbar from "./Components/Navbar/Navbar";
 import Footer from "./Components/Footer/Footer";
-import Services from "./Components/Content/ServicesAgora/service"
+
 
 function App() {
   return (
     <div className='App'>
       <Navbar/>
       <Slider/>
-      <Services/>
       <Footer/>
     </div>
   );
--- src/Components/Content/Valeurs/valeur.js
+++ src/Components/Content/Valeurs/valeur.js
@@ -1,8 +1,44 @@
 import React from 'react'
-
 function valeur() {
   return (
-    <div>valeur</div>
+    
+<section class="slider">
+  <div class="slider__inner container">
+    <div class="slider__sidebar">
+      <div class="slider__head">
+        <div class="slider__title">H3llo D3nver</div>
+        <p class="slider__subtitle">Author: Pieter van de Sande</p>
+      </div>
+      <div class="slider__head">
+        <div class="slider__title">Lurking doubts</div>
+        <p class="slider__subtitle">Author: Dimitar Belchev</p>
+      </div>
+      <div class="slider__head">
+        <div class="slider__title">Geniuses are not born</div>
+        <p class="slider__subtitle">Author: Rahul Bhogal</p>
+      </div>
+      <div class="slider__head">
+        <div class="slider__title">Twilight is coming...</div>
+        <p class="slider__subtitle">Author: Cris DiNoto</p>
+      </div>
+      <div class="slider__head">
+        <div class="slider__title">Sweet dreams</div>
+        <p class="slider__subtitle">Author: Clark Van Der Beken</p>
+      </div>
+    </div>
+    <div class="slider__preview">
+      <div class="slider__preview-item"> </div>
+      <div class="slider__preview-item"> </div>
+      <div class="slider__preview-item"> </div>
+      <div class="slider__preview-item"> </div>
+      <div class="slider__preview-item"> </div>
+    </div>
+    <div class="slider__controls">
+      <button class="slider__controls-btn btn__down btn"><span class="btn__down-arrow"></span></button>
+      <button class="slider__controls-btn btn__up btn"><span class="btn__up-arrow"></span></button>
+    </div>
+  </div>
+</section>
   )
 }
 
