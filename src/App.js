fa7a8d1ca1daa01a0fdb2eda57e23f82e35d8caf
akkanour <akkaa01001@gmail.com>
2022-11-23 18:10:34 +0100

    add routing with path

--- src/App.js
+++ src/App.js
@@ -12,13 +12,19 @@ import Partenaire from "./Components/Content/Partenaires/partenaire";
 
 function App() {
   return (
-    <div className='App'>
+    <Router>
+      <Navbar/>
+      <Routes>
+        <Route exact path="/" element={<Slider/>}/>
+        <Route exact path="/solution" element={<Solution/>}/>
+        <Route exact path="/partenaire" element={<Partenaire/>}/>
+
+      </Routes>
+      <Footer/>
+    </Router>
     
-    <Navbar/>
-    <Slider/>
-    <Solution/>
-    <Footer/>
-    </div>
+  
+    
   );
 }
 
