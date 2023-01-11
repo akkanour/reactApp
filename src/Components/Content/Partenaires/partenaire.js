fdc3a604015c9208160d675c438b59aa7143ee3c
akkanour <akkaa01001@gmail.com>
2023-01-11 13:36:04 +0100

    png to jpg

--- src/Components/Content/Partenaires/partenaire.js
+++ src/Components/Content/Partenaires/partenaire.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import "./partenaire.css";
 import images from './images.json';
-
+import images_produits from './images_produits.json'
 
 function valeur() {
   return (
@@ -26,15 +26,15 @@ function valeur() {
           </div>
         </div>
       </div>
-      
+      NOS PRODUITS 
       <div class="container py-5">
           <div class="row my-5">
             <div class="col-12">
-              <div class="marquee-full-width">
+              <div class="marquee">
                 <div class="marquee-box">
                   <div class="marquee">
                   
-                  {images.map((item) => (
+                  {images_produits.map((item) => (
                       <img src={process.env.PUBLIC_URL +item.url} alt={item.title} />
                       ))}
                   
