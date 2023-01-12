164513640c5d6580329a95508f4325477901daed
tahzima <tahzima.i@gmail.com>
2023-01-12 23:09:04 +0100

    some modification

--- src/Components/Content/OffresEmploi/offreEmploi.js
+++ src/Components/Content/OffresEmploi/offreEmploi.js
@@ -1,24 +1,32 @@
 import React from "react";
-import './offreEmploi.css';
+import "./offreEmploi.css";
 
 function offreEmploi() {
   return (
     <div class="card cardP">
       <div class="card-body">
-        <h5 class="card-title"><img class="symbol" src={process.env.PUBLIC_URL + "../img/offre.jpg"} alt="Offres Emploi"/></h5>
-        <p class="card-text">Aucune offre n'est disponible pour l'instant.</p>
-        <p class="card-text">
-          Si l'une des offres suivantes correspond à votre profile, merci de
-          nous faire parvenir votre C.V + lettre de motivation
-        </p>
-        <p class="card-text">
-          Nos coordonnées : <br />
-          Adresse postale : 227, Bd GHANDI, n 2 20200 Casablanca, Maroc
-          <br />
-          E-mail : information@agora-technology.com
-          <br />
-          Fax : +(212) 522-989-026
-        </p>
+        <h5 class="card-title">
+        <img
+          class="maImage"
+          src={process.env.PUBLIC_URL + "../PartenaireProduit/offreEmploi.jpg"}
+          alt="Offres Emploi"
+        />
+        </h5>
+        <div class="test">
+          <p class="card-text">Aucune offre n'est disponible pour l'instant.</p>
+          <p class="card-text">
+            Si l'une des offres suivantes correspond à votre profile, merci de
+            nous faire parvenir votre C.V + lettre de motivation
+          </p>
+          <p class="card-text">
+            Nos coordonnées : <br />
+            Adresse postale : 227, Bd GHANDI, n 2 20200 Casablanca, Maroc
+            <br />
+            E-mail : information@agora-technology.com
+            <br />
+            Fax : +(212) 522-989-026
+          </p>
+        </div>
       </div>
     </div>
   );
