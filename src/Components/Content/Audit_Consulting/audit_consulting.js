ee4df2fb86a881d65dcd4f0136ac5e42bc738bb4
tahzima <tahzima.i@gmail.com>
2023-01-12 18:55:40 +0100

    audit & consulting

--- src/Components/Content/Audit_Consulting/audit_consulting.js
+++ src/Components/Content/Audit_Consulting/audit_consulting.js
@@ -1,60 +1,61 @@
-import React from 'react'
+import React from "react";
+import "./audit_consulting.css";
+
 
 function audit_consulting() {
   return (
-    <div>
-    <p>
-    Audit Consulting
-  </p>
-  <p>
-  Suite à révolution des Systèmes d'informations, AGORA TECHNOLOGY a développé l'axe audit et consulting dans le but d'accompagner les entreprises dans leurs projets et de les assister dans tous les stades de leur mise en
-  
-  Nos consultants experts mettent leur savoir-faire et experience-terrain à votre disposition pour vos proposer les solutions adéquates afin d'atteindre vos esperassions et les objectifs de vos projets.
-  
-  Notre démarche de gestion des projets d'audit et consulting s'appuient
-  
-  sur les phases suivantes :
-  
-  Consulting & Audit
-  
-  +Expression des besoins,
-  
-  +Comprendre les objectifs,
-  
-  +Analyser, auditer et recenser l'existant,
-  
-  +Choix de la solution technologique,
-  
-  +Mise en place de maquette de test,
-
-Pilotage de la solution,
-
-Mise en oeuvre de la solution,
-
-Exploitation de la solution,
-
-Optimisation de la solution,
-
-Formation des utilisateurs,
-
-Maintenance de la solution,
-
-
-Dans le cade des missions de consulting AGORA TECHNOLOGY a développé les domaines de compétences suivants:
-
-Migration des plates-formes Microsoft Industrialisation des postes de travail et mise en place de solutions d'automatisation
-
-Déploiement de Microsoft SMS Server et WSUS
-
-Migration des systèmes de messagere Microsoft Exchange
-
-Déploiement et implémentation de Microsoft ISA server
-
-→Déploiement et intégration de solution de haute disponibilité, Microsoft cluster server
-Mise en place des services Microsoft terminal server
-  </p>
+    <div class="madiv">
+      <p class="firstP">Audit Consulting</p>
+      <p>
+        Suite à révolution des Systèmes d'informations, AGORA TECHNOLOGY a
+        développé l'axe audit et consulting dans le but d'accompagner les
+        entreprises dans leurs projets et de les assister dans tous les stades
+        de leur mise en oeuvre.
+      </p>
+      <p>
+        Nos consultants experts mettent leur savoir-faire et experience-terrain
+        à votre disposition pour vos proposer les solutions adéquates afin
+        d'atteindre vos esperassions et les objectifs de vos projets.
+      </p>
+      <br/>
+      <p>
+        <b>
+          Notre démarche de gestion des projets d'audit et consulting s'appuient
+          sur les phases suivantes :
+        </b>
+      </p> 
+      <br/> 
+      <ul type="square">
+        <li>Expression des besoins</li>
+        <li>Comprendre les objectifs</li>
+        <li>Analyser, auditer et recenser l'existant</li>
+        <li>Choix de la solution technologique</li>
+        <li>Mise en place de maquette de test</li>
+        <li>Pilotage de la solution</li>
+        <li>Mise en oeuvre de la solution</li>
+        <li>Exploitation de la solution</li>
+        <li>Optimisation de la solution</li>
+        <li>Formation des utilisateurs</li>
+        <li>Maintenance de la solution</li>
+      </ul>
+      <br/>
+      <p>
+        Dans le cade des missions de consulting AGORA TECHNOLOGY a développé les
+        domaines de compétences suivants:
+      </p>
+      <br/>
+      <ul type="square">
+        <li>Migration des plates-formes Microsoft</li>
+        <li>Industrialisation des postes de travail et mise en place de solutions d'automatisation</li>
+        <li>Déploiement de Microsoft SMS Server et WSUS</li>
+        <li>Migration des systèmes de messagere Microsoft Exchange</li>
+        <li>Déploiement et implémentation de Microsoft ISA server</li>
+        <li>Déploiement et intégration de solution de haute disponibilité, Microsoft cluster server</li>
+        <li>Mise en place des services Microsoft terminal server</li>
+      </ul>
+      <br/>
     </div>
-  )
+  );
 }
 
 export default audit_consulting;
